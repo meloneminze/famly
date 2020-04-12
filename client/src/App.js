b18c1291bc7b7b8c02187513e8425f94257bb8f2
Melanie Turek <36224722+meloneminze@users.noreply.github.com>
2020-04-12 14:41:45 +0200

    First steps in programming and color theming (#8)
    * Delete unrelevant files
    
    * Update app and index file
    
    * Prepared themes for different modes
    
    * Install emotion-theming
    
    * Add GlobalStyles
    
    * Update themeprovider in app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './components/GlobalStyles';
+import { ThemeProvider } from 'emotion-theming';
+import daylight from './themes/daylight';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={daylight}>
+      <GlobalStyles />
+      <div>Hello World</div>
+    </ThemeProvider>
   );
 }
 
