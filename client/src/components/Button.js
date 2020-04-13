1a39f9f1bedf47b3a3867270dd94440df7f40423
meloneminze <melanie@derrabus.de>
2020-04-13 21:48:24 +0200

    Create different buttons in stories

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -4,9 +4,10 @@ const Button = styled.button`
   background-color: #45bfd9;
   color: #4d4d4d;
   text-align: center;
-  border: #ffbe68;
+  border: solid;
+  border-color: #ffbe68;
   border-radius: 20px;
-  border-width: medium;
+  border-width: 2px;
   box-shadow: 0px 3px 6px rgba(172, 109, 26, 56);
   font-size: 14px;
   font-family: 'Roboto Mono', monospace;
--- client/src/stories/Button.stories.js
+++ client/src/stories/Button.stories.js
@@ -8,3 +8,12 @@ export default {
 export const loginButton = () => (
   <Button onClick={action('link to Login')}>Anmelden</Button>
 );
+export const signinButton = () => (
+  <Button onClick={action('link to Login')}>Registrieren</Button>
+);
+export const saveButton = () => (
+  <Button onClick={action('link to Login')}>Speichern</Button>
+);
+export const removeButton = () => (
+  <Button onClick={action('link to Login')}>Verwerfen</Button>
+);
