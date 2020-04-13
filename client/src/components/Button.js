38816774415e877b188627e4e25c9da2b3a25e6a
meloneminze <melanie@derrabus.de>
2020-04-13 14:37:49 +0200

    Change values at button.js

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,14 +1,15 @@
 import styled from '@emotion/styled';
 
 const Button = styled.button`
-  background-color: ${(props) => props.theme.colors.buttonPrimary};
-  color: ${(props) => props.theme.colors.textPrimary};
+  background-color: #45bfd9;
+  color: #4d4d4d;
   text-align: center;
-  border: ${(props) => props.theme.colors.light};
+  border: #ffbe68;
   border-radius: 20px;
   border-width: medium;
   box-shadow: 0px 3px 6px rgba(172, 109, 26, 56);
   font-size: 14px;
+  font-family: 'Roboto Mono', monospace;
 `;
 
 export default Button;
