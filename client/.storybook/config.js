2acebb55c574360bd3053e0991524d4f42ce92d3
meloneminze <melanie@derrabus.de>
2020-04-14 22:16:52 +0200

    update pullrequest comments

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,9 +1,22 @@
-import { addDecorator } from '@storybook/react';
-import themeDecorator from './themeDecorator';
-import { addParameters } from '@storybook/react';
+import { addDecorator, addParameters, configure } from '@storybook/react';
+import GlobalStyles from '../src/components/GlobalStyles';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import themeDecorator from './themeDecorator';
+import React from 'react';
+import styled from '@emotion/styled';
+const Main = styled.div`
+  margin: 10px;
+`;
 
+const GlobalStylesDecorator = (storyFn) => (
+  <Main>
+    <GlobalStyles />
+    {storyFn()}
+  </Main>
+);
+addDecorator(GlobalStylesDecorator);
 addDecorator(themeDecorator);
 addParameters({
   viewport: { viewports: INITIAL_VIEWPORTS, defaultViewport: 'iphone6' },
 });
+configure(require.context('../src/stories', true, /\.stories\.js$/), module);
--- client/.storybook/daylightTheme.js
+++ client/.storybook/daylightTheme.js
@@ -5,16 +5,10 @@ export default create({
 
   textPrimaryColor: '#4D4C4C',
   textSecondaryColor: '#707070',
-  textThirdColor: '#FFFFFF',
+  textTertiaryColor: '#FFFFFF',
   backgroundPrimaryColor: '#00A8CC',
   buttonPrimaryColor: '#45BFD9',
   warningColor: '#FEA630',
   lightColor: '#FFBE68',
   inputColor: '#EFEAEA',
-
-  colorPrimary: 'hotpink',
-  colorSecondary: 'deepskyblue',
-
-  brandTitle: 'My famly storybook',
-  brandUrl: 'https://github.com/meloneminze/famly',
 });
