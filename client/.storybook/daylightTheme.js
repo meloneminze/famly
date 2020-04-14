36e72266c57f99f13c0dc0865eabe398b588c88b
meloneminze <melanie@derrabus.de>
2020-04-14 14:01:58 +0200

    Created two themes and manager.js

---
+++ client/.storybook/daylightTheme.js
@@ -0,0 +1,20 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  textPrimaryColor: '#4D4C4C',
+  textSecondaryColor: '#707070',
+  textThirdColor: '#FFFFFF',
+  backgroundPrimaryColor: '#00A8CC',
+  buttonPrimaryColor: '#45BFD9',
+  warningColor: '#FEA630',
+  lightColor: '#FFBE68',
+  inputColor: '#EFEAEA',
+
+  colorPrimary: 'hotpink',
+  colorSecondary: 'deepskyblue',
+
+  brandTitle: 'My famly storybook',
+  brandUrl: 'https://github.com/meloneminze/famly',
+});
---
+++ client/.storybook/manager.js
@@ -0,0 +1,8 @@
+import { addons } from '@storybook/addons';
+import { daylightTheme } from '@storybook/theming';
+import { moonlightTheme } from '@storybook/theming';
+
+addons.setConfig({
+  theme: daylightTheme,
+  theme: moonlightTheme,
+});
