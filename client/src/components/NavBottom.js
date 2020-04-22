42c22baa72507a04b9382d3579ee11f7a6f239e9
meloneminze <melanie@derrabus.de>
2020-04-22 21:50:17 +0200

    Add navBottom file with storybook story

---
+++ client/src/components/NavBottom.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+
+const NavContainer = styled.ul`
+display: flex;
+justify-content: center;
+position: fixed;
+align-items: center;
+align-content: space-between;
+list-style: none;
+height: 10%;
+width= 100%;
+border-top: 0.6rem solid #4d4d4d;
+`;
+
+const NavItem = styled.li`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  max-width: 50%;
+  cursor: pointer;
+`;
+
+const NavBottom = ({ links, active, onItemClick }) => {
+  return (
+    <NavContainer>
+      {links.map((link) => {
+        <NavItem
+          key={link.label}
+          active={active === link.label}
+          onClick={() => onItemClick(link.label)}
+        >
+          <link.Icon active={active === link.label} />
+        </NavItem>;
+      })}
+    </NavContainer>
+  );
+};
+
+NavBottom.propTypes = {
+  links: PropTypes.array,
+  active: PropTypes.string,
+  onItemClick: PropTypes.func,
+};
+export default NavBottom;
