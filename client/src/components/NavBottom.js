cc7f5f74d10d70f7a0379461cd5091ddd3b7f87e
meloneminze <melanie@derrabus.de>
2020-04-24 10:08:02 +0200

    Add basic structure

--- client/src/components/NavBottom.js
+++ client/src/components/NavBottom.js
@@ -1,47 +1,57 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 const NavContainer = styled.ul`
 display: flex;
-justify-content: center;
-position: fixed;
-align-items: center;
+// justify-content: center;
+// align-items: center;
 align-content: space-between;
 list-style: none;
 height: 10%;
 width= 100%;
-border-top: 0.6rem solid #4d4d4d;
+border-top: 0.2rem solid #4d4d4d;
 `;
 
 const NavItem = styled.li`
   display: flex;
   flex: 1;
+  align-content: space-between;
   justify-content: center;
   align-items: center;
   max-width: 50%;
   cursor: pointer;
 `;
 
-const NavBottom = ({ links, active, onItemClick }) => {
+const NavBottom = () => {
   return (
     <NavContainer>
-      {links.map((link) => {
-        <NavItem
-          key={link.label}
-          active={active === link.label}
-          onClick={() => onItemClick(link.label)}
-        >
-          <link.Icon active={active === link.label} />
-        </NavItem>;
-      })}
+      <NavItem>Kind</NavItem>
+      <NavItem>Home</NavItem>
     </NavContainer>
   );
 };
-
-NavBottom.propTypes = {
-  links: PropTypes.array,
-  active: PropTypes.string,
-  onItemClick: PropTypes.func,
-};
 export default NavBottom;
+
+// const NavBottom = ({ links, active, onItemClick }) => {
+//   return (
+//     <NavContainer>
+//       {links.map((link) => {
+//         <NavItem
+//           key={link.label}
+//           active={active === link.label}
+//           onClick={() => onItemClick(link.label)}
+//         >
+//           <link.Icon active={active === link.label} />
+//         </NavItem>;
+//       })}
+//     </NavContainer>
+//   );
+// };
+
+// NavBottom.propTypes = {
+//   links: PropTypes.array,
+//   active: PropTypes.string,
+//   onItemClick: PropTypes.func,
+// };
+// export default NavBottom;
