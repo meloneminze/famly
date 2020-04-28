3fb76904efa7289179048fa7c5c53ea3af1ff888
meloneminze <melanie@derrabus.de>
2020-04-28 21:26:10 +0200

    Create a data field ith story in storybook

---
+++ client/src/stories/SimpleDataField.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SimpleDataField from '../components/SimpleDataField';
+
+export default {
+  title: 'SimpleDataField',
+};
+
+export const DataField = () => {
+  return <SimpleDataField />;
+};
