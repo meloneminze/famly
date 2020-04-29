0cf5125ec0a1b3b0527ba71cf9bb35f3f8958730
Melanie Turek <36224722+meloneminze@users.noreply.github.com>
2020-04-29 14:14:28 +0200

    Create a data field with story in storybook (#34)
    * Create a data field ith story in storybook
    
    * Update changes from pull request comments

---
+++ client/src/components/MemberInfoInput.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const TextareaContainer = styled.div`
+  flex-flow: row;
+  flex-wrap: wrap;
+  background-color: ${(props) => props.theme.colors.input};
+  border-radius: 1.25rem;
+  border: none;
+  padding: 1rem;
+  outline: none;
+`;
+
+const Headline = styled.input`
+background-color: ${(props) => props.theme.colors.input};
+color: ${(props) => props.theme.colors.textSecondary}
+font-weight: bold;
+outline: none;
+cursor: pointer;
+width: 100%;
+border: none;
+border-bottom: dotted #707070;
+::placeholder {
+    color: ${(props) => props.theme.colors.textSecondary};
+`;
+
+const Textarea = styled.textarea`
+background-color: ${(props) => props.theme.colors.input};
+  color: ${(props) => props.theme.colors.textPrimary};
+  border: none;
+  outline: none;
+  cursor: pointer;
+  width: 100%;
+  resize: none;
+  wrap: "off";
+  ::placeholder {
+    color: ${(props) => props.theme.colors.textSecondary};
+`;
+
+const SimpleDataField = () => {
+  return (
+    <TextareaContainer>
+      <Headline
+        placeholder="Adresse der Schule"
+        minLength={1}
+        maxLength={50}
+        // size="65"
+      ></Headline>
+      <Textarea
+        placeholder="Aquariumstraße 42"
+        maxLength={200}
+        rows={3}
+        // cols={60}
+      ></Textarea>
+    </TextareaContainer>
+  );
+};
+
+export default SimpleDataField;
---
+++ client/src/stories/MemberInfoInput.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MemberInfoInput from '../components/MemberInfoInput';
+
+export default {
+  title: 'MemberInfoInput',
+};
+
+export const DataField = () => {
+  return <MemberInfoInput />;
+};
