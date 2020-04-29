3d3f927a6e66b43a876b96e37592149ef627d78c
meloneminze <melanie@derrabus.de>
2020-04-29 10:14:09 +0200

    update branch with pull request comments

--- client/src/stories/NavBottom.stories.js
+++ client/src/stories/NavBottom.stories.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import NavBottom from '../components/NavBottom';
-// import Child from '../assets/face-24px.svg';
-// import Home from '../assets/home-24px.svg';
 
 export default {
   title: 'Bottom Navigation',
@@ -9,16 +7,5 @@ export default {
 };
 
 export const Nav = () => {
-  //   const [active, setActive] = useState('Children');
-
-  return (
-    <NavBottom />
-    //       links={[
-    //         { label: 'Children', Icon: Child },
-    //         { label: 'Household', Icon: Home },
-    //       ]}
-    //       value={active}
-    //       onItemClick={(page) => setActive(page)}
-    //     />
-  );
+  return <NavBottom />;
 };
