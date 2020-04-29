83bdabd1c31cc8afc32be99e49b02f2a9bbfa500
Melanie Turek <36224722+meloneminze@users.noreply.github.com>
2020-04-29 13:45:26 +0200

    update branch with pull request comments (#35)

--- client/src/components/NavBottom.js
+++ client/src/components/NavBottom.js
@@ -4,8 +4,6 @@ import { Link, useLocation } from 'react-router-dom';
 
 const NavContainer = styled.ul`
   display: flex;
-  // justify-content: center;
-  // align-items: center;
   list-style-type: none;
   text-decoration: none;
   margin: 0;
@@ -60,7 +58,6 @@ const NavBottom = () => {
             height="24"
             fill="#4D4D4D"
             viewBox="0 0 24 24"
-            active={location.pathname === '/'}
           >
             <path d="M9 11.75c-.69 0-1.25.56-1.25 1.25s.56 1.25 1.25 1.25 1.25-.56 1.25-1.25-.56-1.25-1.25-1.25zm6 0c-.69 0-1.25.56-1.25 1.25s.56 1.25 1.25 1.25 1.25-.56 1.25-1.25-.56-1.25-1.25-1.25zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8 0-.29.02-.58.05-.86 2.36-1.05 4.23-2.98 5.21-5.37C11.07 8.33 14.05 10 17.42 10c.78 0 1.53-.09 2.25-.26.21.71.33 1.47.33 2.26 0 4.41-3.59 8-8 8z" />
           </SVG>
@@ -74,7 +71,6 @@ const NavBottom = () => {
             height="24"
             viewBox="0 0 24 24"
             fill="#4D4D4D"
-            active={location.pathname === '/'}
           >
             <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
           </SVG>
