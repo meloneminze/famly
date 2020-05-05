8287e15f54288e9fe7a49dc8c4de1b418c8497ca
meloneminze <melanie@derrabus.de>
2020-05-05 13:46:10 +0200

    Update placeholder font to bold

---
+++ client/src/components/CreateMemberInfoInput.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const CreateTextareaContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  background-color: ${(props) => props.theme.colors.input};
+  border-radius: 20px;
+  border: none;
+  padding: 16px;
+  outline: none;
+  box-shadow: 0px 3px 6px;
+`;
+
+const CreateHeadline = styled.input`
+background-color: ${(props) => props.theme.colors.input};
+color: ${(props) => props.theme.colors.textSecondary}
+font-weight: bold;
+outline: none;
+cursor: pointer;
+border: none;
+width: 100%;
+border-bottom: dotted #707070;
+::placeholder {
+    color: ${(props) => props.theme.colors.textSecondary};
+    font-weight: bold;
+`;
+
+const CreateTextarea = styled.textarea`
+background-color: ${(props) => props.theme.colors.input};
+  color: ${(props) => props.theme.colors.textPrimary};
+  border: none;
+  outline: none;
+  cursor: pointer;
+  resize: none;
+  ::placeholder {
+    color: ${(props) => props.theme.colors.textSecondary};
+`;
+
+const CreateMemberInfoInput = () => {
+  return (
+    <CreateTextareaContainer>
+      <CreateHeadline
+        placeholder="Adresse der Schule"
+        minLength="1"
+        maxLength="50"
+      ></CreateHeadline>
+      <CreateTextarea
+        placeholder="Aquariumstraße 42"
+        maxLength="200"
+        rows="3"
+        cols="45"
+      ></CreateTextarea>
+    </CreateTextareaContainer>
+  );
+};
+
+export default CreateMemberInfoInput;
---
+++ client/src/components/DisplayMemberInfoInput.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const TextareaContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  background-color: ${(props) => props.theme.colors.input};
+  border-radius: 20px;
+  border: none;
+  padding: 16px;
+  outline: none;
+  box-shadow: 0px 3px 6px;
+`;
+
+const Headline = styled.input`
+background-color: ${(props) => props.theme.colors.input};
+color: ${(props) => props.theme.colors.textSecondary}
+font-weight: bold;
+outline: none;
+cursor: pointer;
+border: none;
+width: 100%;
+::placeholder {
+    color: ${(props) => props.theme.colors.textSecondary};
+    font-weight: bold;
+`;
+
+const Textarea = styled.textarea`
+background-color: ${(props) => props.theme.colors.input};
+  color: ${(props) => props.theme.colors.textPrimary};
+  border: none;
+  outline: none;
+  cursor: pointer;
+  resize: none;
+  ::placeholder {
+    color: ${(props) => props.theme.colors.textSecondary};
+`;
+
+const MemberInfoInput = () => {
+  return (
+    <TextareaContainer>
+      <Headline
+        placeholder="Adresse der Schule"
+        minLength="1"
+        maxLength="50"
+      ></Headline>
+      <Textarea
+        placeholder="Aquariumstraße 42"
+        maxLength="200"
+        rows="3"
+        cols="45"
+      ></Textarea>
+    </TextareaContainer>
+  );
+};
+
+export default MemberInfoInput;
