66c7f9c414763aeba180483551203fe786194b7f
meloneminze <melanie@derrabus.de>
2020-05-05 20:14:25 +0200

    Add displayhousehold page and story

---
+++ client/src/pages/DisplayHousehold.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function DisplayHousehold() {
+  return (
+    <div>
+      Mein Haushalt <Link to="/">Kinder</Link>
+    </div>
+  );
+}
+
+export default DisplayHousehold;
---
+++ client/src/stories/DisplayHousehold.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import DisplayHousehold from '../pages/DisplayHousehold';
+
+export default {
+  title: 'Pages',
+};
+
+export const DisplayHouseholdPage = () => <DisplayHousehold />;
