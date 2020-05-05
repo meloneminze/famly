4a7903e050ca6e6e68deb439bf169085ce64937e
meloneminze <melanie@derrabus.de>
2020-05-05 12:44:43 +0200

    Update routing to different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,47 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import GlobalStyles from './components/GlobalStyles';
 import { ThemeProvider } from 'emotion-theming';
-import styled from '@emotion/styled';
 import daylight from './themes/daylight';
 import Children from './pages/Children';
 import Household from './pages/Household';
-import AppHeader from './components/AppHeader';
 
-const Main = styled.main`
-  display: flex;
-`;
+import CreateChild from './pages/CreateChild';
 
 function App() {
   return (
     <ThemeProvider theme={daylight}>
       <Router>
         <GlobalStyles />
-        <AppHeader />
-        <Main>
-          <Switch>
-            <Route path="/household">
-              <Household />
-            </Route>
-            <Route path="/">
-              <Children />
-            </Route>
-          </Switch>
-        </Main>
+
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/children" />
+          </Route>
+          <Route exact path="/login">
+            <div>Login</div>
+          </Route>
+          <Route exact path="/register">
+            <div>Register</div>
+          </Route>
+          <Route exact path="/children">
+            <Children />
+          </Route>
+          <Route exact path="/children/create">
+            <CreateChild />
+          </Route>
+          <Route exact path="/children/:id">
+            <div>Child</div>
+          </Route>
+          <Route path="/household">
+            <Household />
+          </Route>
+        </Switch>
       </Router>
     </ThemeProvider>
   );
